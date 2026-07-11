4a0cbdd7155c1beaecf12a862fffedf7b9cd2336
Nafiur Rahman Shafi <shafi21092003@gmail.com>
2026-07-12 03:57:52 +0600

    feat: wrap app in StrictMode and ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
+import { ErrorBoundary } from "./app/ErrorBoundary.tsx";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
+);
